c2592a34849b1b968b5b1df7269b1bf8cf4ac0a7
Michael Ninh <michaelninh@michale-ninh-macbook-pro.local>
2018-09-11 19:31:39 -0700

    Initial Commit

---
+++ youtube_practiceTests/youtube_practiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  youtube_practiceTests.swift
+//  youtube_practiceTests
+//
+//  Created by Michael Ninh on 9/11/18.
+//  Copyright © 2018 Michael Ninh. All rights reserved.
+//
+
+import XCTest
+@testable import youtube_practice
+
+class youtube_practiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
